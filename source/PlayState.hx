3ec1c8e98dbf6dc4bb84f173c9c9bea0f32f8d53
gaminglolkacoak <95685437+gaminglolkacoak@users.noreply.github.com>
2021-12-07 19:51:45 +0700

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -95,7 +95,6 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
 			menuItem.screenCenter(X);
-			menuItem.x += 300;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
